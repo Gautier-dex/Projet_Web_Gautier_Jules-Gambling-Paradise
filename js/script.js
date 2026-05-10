6ace81ecf2afea4bfb2ddf5e31ac5930045e5f73
Jules Termoz <julestermoz@gmail.com>
2026-05-10 16:41:31 +0200

    js pour rejoindre

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 function inscription (){
-        localStorage.setItem('isRegistered', 'true');
-        alert('Inscription réussie ! Vous avez maintenant accès au contenu premium.');
+    let username = document.getElementById("Username").value;
+    let email = document.getElementById("Email").value;
+    let password = document.getElementById("Password").value;
+    let statut = document.getElementById("Statut").value;
+    if (username === ""  || email === "" || password === "" || statut === "option1"){
+        alert("Vous n'avez pas remplis tous les champs !");
+        return;
+    }
+    localStorage.setItem('isRegistered', 'true');
+    alert('Inscription réussie ! Vous avez maintenant accès au contenu premium.');
 }
 
 
 function uninscription (){
+    if (localStorage.getItem('isRegistered') === 'false'){
+        alert("Vous n'êtes pas encore inscrit !");
+        return;
+    }
     localStorage.setItem('isRegistered', 'false');
     alert("Désinscription réussie ! Vous n'avez maintenant plus accès au contenu premium.");
 }
